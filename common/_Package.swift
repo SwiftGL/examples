747b085df622c7de4af73fc0a2dc751869a3f393
Justin Kolb <franticapparatus@gmail.com>
2017-08-04 13:42:31 -0500

    Make use of new cross platform CGLFW3

--- common/_Package.swift
+++ common/_Package.swift
@@ -3,20 +3,9 @@ import PackageDescription
 let package = Package(
     name: "main",
     dependencies: [
-        .Package(url: "https://github.com/SwiftGL/OpenGL.git", majorVersion: 1),
+        .Package(url: "https://github.com/SwiftGL/OpenGL.git", majorVersion: 2),
         .Package(url: "https://github.com/SwiftGL/Math.git", majorVersion: 1),
-        .Package(url: "https://github.com/SwiftGL/Image.git", majorVersion: 1)
+        .Package(url: "https://github.com/SwiftGL/Image.git", majorVersion: 1),
+        .Package(url: "https://github.com/SwiftGL/CGLFW3.git", majorVersion: 2),
     ]
 )
-
-#if os(Linux)
-    package.dependencies.append(
-        Package.Dependency.Package(url: "https://github.com/SwiftGL/CGLFW3Linux.git", majorVersion: 1)
-        // If your distro renamed the library to "glfw" (no 3) use this instead:
-        // Package.Dependency.Package(url: "https://github.com/SwiftGL/CGLFWLinux.git", majorVersion: 1)
-    )
-#else
-    package.dependencies.append(
-        Package.Dependency.Package(url: "https://github.com/SwiftGL/CGLFW3.git", majorVersion: 1)
-    )
-#endif
