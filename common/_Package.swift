04578c7746dcec510250d70eeaae0501ba0fa28a
Justin Kolb <franticapparatus@gmail.com>
2017-09-23 10:06:57 -0500

    Update for Swift 4

--- common/_Package.swift
+++ common/_Package.swift
@@ -1,11 +1,19 @@
+// swift-tools-version:4.0
+
 import PackageDescription
 
 let package = Package(
-    name: "main",
+    name: "example",
+    products: [
+        .executable(name: "example", targets: ["example"]),
+    ],
     dependencies: [
-        .Package(url: "https://github.com/SwiftGL/OpenGL.git", majorVersion: 2),
-        .Package(url: "https://github.com/SwiftGL/Math.git", majorVersion: 1),
-        .Package(url: "https://github.com/SwiftGL/Image.git", majorVersion: 1),
-        .Package(url: "https://github.com/SwiftGL/CGLFW3.git", majorVersion: 2),
+        .package(url: "https://github.com/SwiftGL/OpenGL.git", from: "3.0.0"),
+        .package(url: "https://github.com/SwiftGL/Math.git", from: "2.0.0"),
+        .package(url: "https://github.com/SwiftGL/Image.git", from: "2.0.0"),
+        .package(url: "https://github.com/SwiftGL/CGLFW3.git", from: "2.0.0"),
+    ],
+    targets: [
+        .target(name: "example", dependencies: ["SGLMath", "SGLImage", "SGLOpenGL"], path: "."),
     ]
 )
